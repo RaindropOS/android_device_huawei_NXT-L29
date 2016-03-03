8090735c21c7bd83c006b0c7a261e43aa8569cf0
lj50036 <jl52741@gmail.com>
2016-03-02 19:32:17 -0600

    Some needed missing info:
    Add partition sizes
    Cleanup boot_args format

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,15 +23,17 @@ TARGET_BOOTLOADER_BOARD_NAME := NEXT,NXT-L29
 # Kernel
 TARGET_PREBUILT_RECOVERY_KERNEL := device/huawei/next/kernel
 BOARD_KERNEL_CMDLINE  := 'mmcparts=mmcblk0:p1(vrl),p2(vrl_backup),p6(modemnvm_factory),p9(splash),p10(modemnvm_backup),p11(modemnvm_img),p12(modemnvm_system),p14(3rdmodemnvm),p15(3rdmodemnvmbkp),p18(modem_om),p21(modemnvm_update),p31(modem),p32(modem_dsp),p35(3rdmodem) loglevel=4 androidboot.selinux=enforcing'
-BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_BASE     := 0x00478000
-BOARD_KERNEL_OFFSET   := 0x00008000
-BOARD_RAMDISK_OFFSET  := 0x07b88000
-BOARD_TAGS_OFFSET     := 0x07588000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --tags_offset 0x07588000
+BOARD_KERNEL_PAGESIZE := 2048
 
-BOARD_MKBOOTIMG_ARGS += --kernel_offset "$(BOARD_KERNEL_OFFSET)"
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset "$(BOARD_RAMDISK_OFFSET)"
-BOARD_MKBOOTIMG_ARGS += --tags_offset "$(BOARD_TAGS_OFFSET)"
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 26935820288
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
 
 # USB mass storage
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
@@ -44,7 +46,6 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TW_NO_USB_STORAGE := true
-TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 TW_BRIGHTNESS_PATH := "/sys/devices/platform/hisi_fb.1048577/leds/lcd_backlight0/brightness"
 TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/bq_bci_battery.1/power_supply/Battery"
