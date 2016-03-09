c57410b1abad4d8be1cd44de6a387a7c2ffa0c91
lj50036 <jl52741@gmail.com>
2016-03-09 15:51:35 -0600

    WIP: Deivce tree brinup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,24 +1,31 @@
 USE_CAMERA_STUB := true
 
+TARGET_BOARD_PLATFORM := hi3650
+TARGET_BOOTLOADER_BOARD_NAME := hi3650
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
-# CPU
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a15
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a72
+
+BOARD_VENDOR := huawei
+BOARD_VENDOR_PLATFORM := hi3650
+TARGET_SOC := kirin950
+
+TARGET_BOOTLOADER_BOARD_NAME := NEXT,NXT-L29
 
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a15 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a15 -mfloat-abi=softfp
 TARGET_EXTRA_CFLAGS := -mtune=cortex-a15 -mcpu=cortex-a15
-
-BOARD_VENDOR := huawei
-BOARD_VENDOR_PLATFORM := hi3650
-TARGET_BOOTLOADER_BOARD_NAME := hi3650
-TARGET_BOARD_PLATFORM := hi3650
-TARGET_SOC := kirin950
-
-TARGET_BOOTLOADER_BOARD_NAME := NEXT,NXT-L29
 
 # Kernel
 TARGET_PREBUILT_RECOVERY_KERNEL := device/huawei/next/kernel
@@ -27,6 +34,17 @@ BOARD_KERNEL_BASE     := 0x00478000
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --tags_offset 0x07588000
 BOARD_KERNEL_PAGESIZE := 2048
 
+USE_OPENGL_RENDERER := true
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
@@ -34,21 +52,6 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 26935820288
 BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_USERIMAGES_USE_EXT4 := true
-
-# USB mass storage
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
-
-# Recovery
-BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 
-#TWRP
-BOARD_HAS_NO_SELECT_BUTTON := true
-TW_THEME := portrait_hdpi
-RECOVERY_SDCARD_ON_DATA := true
-TW_INCLUDE_CRYPTO := true
-TW_NO_USB_STORAGE := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-TW_BRIGHTNESS_PATH := "/sys/devices/platform/hisi_fb.1048577/leds/lcd_backlight0/brightness"
-TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/bq_bci_battery.1/power_supply/Battery"
-TW_MAX_BRIGHTNESS := 255
+-include vendor/huawei/next/BoardConfigVendor.mk
---
+++ BoardConfigRaindrop.mk
@@ -0,0 +1,12 @@
+# TWRP
+TW_THEME := portrait_hdpi
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_CRYPTO := true
+TW_NO_USB_STORAGE := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+TW_BRIGHTNESS_PATH := "/sys/devices/platform/hisi_fb.1048577/leds/lcd_backlight0/brightness"
+TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/bq_bci_battery.1/power_supply/Battery"
+TW_MAX_BRIGHTNESS := 255
